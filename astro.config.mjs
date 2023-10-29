397590479e3c6a637e9dcb1b6e3e56aa0d25703d
Denrei Keith <denzkei12@gmail.com>
2023-10-29 23:48:30 +0800

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import AstroPWA from "@vite-pwa/astro";
@@ -7,6 +7,7 @@ import AstroPWA from "@vite-pwa/astro";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercel(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
